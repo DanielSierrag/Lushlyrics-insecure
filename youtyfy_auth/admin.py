cc944e084e32417b560ab253ab2c87ff29e1eee8
danielsierrag <danielgreco@2209mail.com>
2025-05-16 12:51:24 -0500

    Replacing default user model to use email based authentication

---
+++ youtyfy_auth/admin.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from django.contrib import admin
+
+# Register your models here.
+
+
+@admin.register(get_user_model())
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "usable_password", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
---
+++ youtyfy_auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YoutyfyAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'youtyfy_auth'
